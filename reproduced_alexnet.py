0a0472db1264fe20b80c48fd0d49613f6da6f950
burningDown <734556742@qq.com>
2019-03-08 22:18:41 +0800

    3

--- reproduced_alexnet.py
+++ reproduced_alexnet.py
@@ -7,6 +7,8 @@ FILE_NAME = "/root/workspace/data_input/cifar-10-batches-bin/data_batch_1.bin"
 class AlexNet:
     network = None
     keep_prob = tf.placeholder(dtype=tf.float32)
+    x = tf.placeholder(dtype=float32)
+    label = tf.placeholder(dtype=float32)
     weights = {
         "w1": tf.Variable(tf.truncated_normal(shape=[11, 11, 3, 96], stddev=0.1)),
         "w2": tf.Variable(tf.truncated_normal(shape=[5, 5, 96, 256], stddev=0.1)),
@@ -40,10 +42,10 @@ class AlexNet:
             .map(map_reshape, num_parallel_calls=100).batch(mini_batch)
         return file_dataset.make_one_shot_iterator().get_next()
 
-    def inference(self, images):
+    def inference(self):
         with tf.name_scope("conv1"):
             # self.convs["conv1"]
-            conv = tf.nn.relu(tf.nn.conv2d(images, self.weights["w1"], [1, 1, 1, 1], 'SAME') + self.bias["b1"])
+            conv = tf.nn.relu(tf.nn.conv2d(self.x, self.weights["w1"], [1, 1, 1, 1], 'SAME') + self.bias["b1"])
             self.layers["conv1"] = tf.nn.max_pool(conv, [1, 3, 3, 1], [1, 2, 2, 1], 'SAME')
         with tf.name_scope("conv2"):
             conv = tf.nn.relu(tf.nn.conv2d(self.layers["conv1"],
@@ -72,30 +74,30 @@ class AlexNet:
             self.layers["softmax"] = tf.nn.softmax(tf.matmul(self.layers["fc2"], self.weights["w8"]) + self.bias["b8"])
         return self.layers["softmax"]
 
-    def loss(self, labels, y):
-        return tf.reduce_mean(-tf.reduce_sum(labels*tf.log(y), axis=[1]))
+    def loss(self, y):
+        return tf.reduce_mean(-tf.reduce_sum(self.label*tf.log(y), axis=[1]))
 
-    def train_step(self, loss, alpha = 0.0001):
+    def train_step(self, loss, alpha=0.0001):
         return tf.train.AdamOptimizer(alpha).minimize(loss)
 
-    def accuracy(self, labels, y):
-        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(labels, 1))
+    def accuracy(self, y):
+        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(self.label, 1))
         return tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 
 def main():
     alex_net = AlexNet()
-    images, labels = alex_net.get_images(10)
-    network = alex_net.inference(images)
-    loss = alex_net.loss(labels, network)
+    network = alex_net.inference()
+    loss = alex_net.loss(network)
     train_step = alex_net.train_step(loss)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for i in range(0, 100):
+        for i in range(0, 500):
             if i % 10 == 0:
-                print(sess.run(loss, feed_dict={alex_net.keep_prob: 1}))
-            sess.run(train_step, feed_dict={alex_net.keep_prob: 0.5})
+                print(i)
+            images, labels = alex_net.get_images(10)
+            sess.run(train_step, feed_dict={alex_net.keep_prob: 0.5, alex_net.x: images, alex_net.label: labels})
 
 
 if __name__ == "__main__":
